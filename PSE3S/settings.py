c0732fdf1b9713e09492d714be16cc3956bba0d5
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2020-11-16 11:50:17 +0000

    Installed required packages and added to requirements.txt and settings.py

--- PSE3S/settings.py
+++ PSE3S/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_forms_bootstrap',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
