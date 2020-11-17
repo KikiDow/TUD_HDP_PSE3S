f5dd7247b2a30c3ed465d02046f688c007a856b2
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2020-11-17 16:28:10 +0000

    Implemented user registration functionality using registration, view_new_account, edit_new_account and delete_account views, urls and templates.

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import url, include
-from .views import logout_view, login_view, home_page
+from .views import logout_view, login_view, home_page, registration, view_new_account, delete_account, edit_new_account
 
 urlpatterns = [
     url(r'^logout_view/$', logout_view, name='logout_view'),
     url(r'^login_view/$', login_view, name='login_view'),
     url(r'^home_page/$', home_page, name='home_page'),
+    url(r'^registration/$', registration, name='registration'),
+    url(r'^(?P<pk>\d+)/$', view_new_account, name='view_new_account'),
+    url(r'^(?P<pk>\d+)/delete_account/$', delete_account, name='delete_account'),
+    url(r'^(?P<pk>\d+)/edit_new_account/$', edit_new_account, name='edit_new_account'),
 ]
--- account/views.py
+++ account/views.py
@@ -48,3 +48,66 @@ def home_page(request):
     Views renders home page to user.
     '''
     return render(request, 'home_page.html')
+    
+@login_required
+def registration(request):
+    '''
+    This view renders the user registration form for users 
+    '''
+    if request.POST:
+        reg_form = RegistrationForm(request.POST)
+        if reg_form.is_valid():
+            reg_form.save()
+            email = reg_form.cleaned_data.get('email')
+            raw_password = reg_form.cleaned_data.get('password1')
+            account = authenticate(email=email, password=raw_password)
+            messages.success(request, "You have successfully registered a new user.")
+            new_account = Account.objects.get(email=email)
+            #current_qtr = getCurrentQtr()
+            #new_ot_per_qtr = OvertimePerQtr(ot_per_qtr_off_id=new_account, ot_per_qtr_qtr_id=current_qtr)
+            #new_ot_per_qtr.save()
+            
+            return redirect('view_new_account', new_account.pk)
+            
+    else:
+        reg_form = RegistrationForm()
+    
+    args = {'reg_form': reg_form}
+    return render(request, 'register.html', args)
+    
+@login_required()
+def view_new_account(request, pk):
+    '''
+    This view shows a supervisor user the user they have just created with options to delete or edit the user.
+    '''
+    newly_created_account = get_object_or_404(Account, pk=pk)
+    newly_created_account.save()
+    return render(request, 'view_new_account.html', {'newly_created_account': newly_created_account})
+    
+@login_required()
+def delete_account(request, pk):
+    '''
+    This view allows supervisor users to delete an account.
+    '''
+    account_for_deletion = Account.objects.get(pk=pk)
+    account_for_deletion.delete()
+    messages.success(request, "You have successfully deleted this account.")
+    return redirect('registration')
+    
+@login_required()
+def edit_new_account(request, pk):
+    '''
+    This view allows a supervisor user to edit an account they have just created. Incase of errors.
+    '''
+    account_for_editing = get_object_or_404(Account, pk=pk) if pk else None
+    if request.method == "POST":
+        edit_account_form = RegistrationForm(request.POST, request.FILES, instance=account_for_editing)
+        if edit_account_form.is_valid():
+            account_for_editing = edit_account_form.save()
+            messages.success(request, 'You have successfully made changes to this account.')
+            return redirect(view_new_account, account_for_editing.pk)
+    else:
+        edit_account_form = RegistrationForm(instance=account_for_editing)
+        
+
+    return render(request, "edit_new_account.html", {'account_for_editing': account_for_editing, 'edit_account_form': edit_account_form})
