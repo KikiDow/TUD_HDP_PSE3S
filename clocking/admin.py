cc5514ebd1444df80858416f0ea233964855ca6b
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2020-11-18 10:57:24 +0000

    Added PersonalDetails and registered with admin via admin.py

--- clocking/admin.py
+++ clocking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Quarter, Shift, RosterSideA, RosterSideB, Roster
+from .models import Quarter, Shift, RosterSideA, RosterSideB, Roster, PersonalDetails
 
 # Register your models here.
 admin.site.register(Quarter)
@@ -7,3 +7,4 @@ admin.site.register(Shift)
 admin.site.register(RosterSideA)
 admin.site.register(RosterSideB)
 admin.site.register(Roster)
+admin.site.register(PersonalDetails)
