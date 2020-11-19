5d4b66e53fb1a5386c4a23189f3290eb07339fd1
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2020-11-19 15:05:13 +0000

    Created view, usl and template to submit csl form. Also corrected fieldset error in submit_block_leave.html.

--- abscences/urls.py
+++ abscences/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import abscences_page
+from .views import abscences_page, submit_csl
 
 urlpatterns = [
-    url(r'^abscences_page/$', abscences_page, name='abscences_page'),    
+    url(r'^abscences_page/$', abscences_page, name='abscences_page'),
+    url(r'^submit_csl/$', submit_csl, name='submit_csl'),
 ]
--- abscences/views.py
+++ abscences/views.py
@@ -9,4 +9,36 @@ from notifications.signals import notify
 # Create your views here.
 @login_required()
 def abscences_page(request):
+    '''
+    This view renders the main page for the abscences section.
+    '''
     return render(request, "abscences_page.html")
+    
+@login_required()
+def submit_csl(request):
+    '''
+    This view renders, server side validates and creates newly submitted csl.
+    '''
+    if request.method == "POST":
+        csl_form = CertifiedSickLeaveForm(request.POST, request.FILES)
+        if csl_form.is_valid():
+            csl_form.instance.csl_officer_id = request.user
+            if csl_form.instance.first_day_of_cert > csl_form.instance.last_day_of_cert:
+                messages.error(request, "The start date for the cert must be before the last date.")
+                return render(request, "submit_csl.html", {'csl_form': csl_form})
+            else:
+                csl_application = csl_form.save()
+                messages.success(request, 'You have successfully submitted a certified sick leave application.')
+                return redirect(view_csl_application, csl_application.pk)
+    else:
+        csl_form = CertifiedSickLeaveForm()
+    return render(request, "submit_csl.html", {'csl_form': csl_form})
+    
+@login_required()
+def view_csl_application(request, pk):
+    '''
+    This view displays a single csl application.
+    '''
+    csl_application = get_object_or_404(CertifiedSickLeave, pk=pk)
+    csl_application.save()
+    return render(request, "view_csl_application.html", {'csl_application': csl_application})
