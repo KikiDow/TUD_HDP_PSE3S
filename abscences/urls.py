21638771a7964ddb9831c96239f5cd3a970f3de6
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2020-11-19 15:35:31 +0000

    Implemented view, url, links and template to view new csl application. Also corrected conditional content jinja statements in view_al_request.html and view_st_request.html

--- abscences/urls.py
+++ abscences/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import abscences_page, submit_csl
+from .views import abscences_page, submit_csl, view_csl_application
 
 urlpatterns = [
     url(r'^abscences_page/$', abscences_page, name='abscences_page'),
     url(r'^submit_csl/$', submit_csl, name='submit_csl'),
+    url(r'^(?P<pk>\d+)/view_csl_application/$', view_csl_application, name='view_csl_application'),
 ]
