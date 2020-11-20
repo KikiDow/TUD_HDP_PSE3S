10be97cbe0a2e808631e3b86755bbf22ca57498f
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2020-11-20 10:59:33 +0000

    Implemented edit allowance request functionality with view, url, edit_allowance_request.html templte file and link on view_allowance_request.html file.

--- overtime/urls.py
+++ overtime/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url, include
-from .views import overtime_page, allowances_page, submit_allowance_request, view_allowance_request
+from .views import overtime_page, allowances_page, submit_allowance_request, view_allowance_request, edit_allowance_request
 
 urlpatterns = [
     url(r'^overtime_page/$', overtime_page, name='overtime_page'),
     url(r'^allowances_page/$', allowances_page, name='allowances_page'),
     url(r'^submit_allowance_request/$', submit_allowance_request, name='submit_allowance_request'),
     url(r'^(?P<pk>\d+)view_allowance_request/$', view_allowance_request, name='view_allowance_request'),
+    url(r'^(?P<pk>\d+)/edit_allowance_request/$', edit_allowance_request, name='edit_allowance_request'),
 ]
--- overtime/views.py
+++ overtime/views.py
@@ -74,3 +74,36 @@ def view_allowance_request(request, pk):
     allow_req_to_view = get_object_or_404(AllowancesRequest, pk=pk)
     allow_req_to_view.save()
     return render(request, "view_allowance_request.html", {'allow_req_to_view': allow_req_to_view})
+    
+@login_required()
+def edit_allowance_request(request, pk):
+    allow_req_for_editing = get_object_or_404(AllowancesRequest, pk=pk) if pk else None
+    if request.method == "POST":
+        edit_allow_req_form = AllowancesRequestForm(request.POST, request.FILES, instance=allow_req_for_editing)
+        if edit_allow_req_form.is_valid():
+            if edit_allow_req_form.instance.claiming_food_for_prisoner_expense == True:
+                if edit_allow_req_form.instance.food_for_prisoner_amount is None:
+                    messages.error(request, "You must include the cost of the prisoner's meal to claim this expense.")
+                    return render(request, "edit_allowance_request.html", {'edit_allow_req_form': edit_allow_req_form})
+                elif edit_allow_req_form.instance.receipt_for_prisoner_food is False:
+                    messages.error(request, "You must provide a photograph of the receipt for the prisoner's meal to claim this expense.")
+                    return render(request, "edit_allowance_request.html", {'edit_allow_req_form': edit_allow_req_form})
+                else:
+                    allow_req_for_editing = edit_allow_req_form.save()
+                    total_claim_amount = 0.0
+                    if allow_req_for_editing.claiming_breakfast_allowance == True:
+                        total_claim_amount += settings.BREAKFAST_ALLOWANCE
+                    if allow_req_for_editing.claiming_dinner_allowance == True:
+                        total_claim_amount += settings.DINNER_ALLOWANCE
+                    if allow_req_for_editing.claiming_tea_allowance == True:
+                        total_claim_amount += settings.TEA_ALLOWANCE
+                    if allow_req_for_editing.claiming_plain_clothes_allowance == True:
+                        total_claim_amount += settings.PLAIN_CLOTHES_ALLOWANCE
+                    if allow_req_for_editing.claiming_food_for_prisoner_expense == True:
+                        total_claim_amount += allow_req_for_editing.food_for_prisoner_amount
+                    allow_req_for_editing.claim_total = total_claim_amount
+                    allow_req_for_editing.save()
+                    return redirect(view_allowance_request, allow_req_for_editing.pk)
+    else:
+        edit_allow_req_form = AllowancesRequestForm(instance=allow_req_for_editing)
+    return render(request, "edit_allowance_request.html", {'allow_req_for_editing': allow_req_for_editing, 'edit_allow_req_form': edit_allow_req_form})
