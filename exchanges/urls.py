5d635d96c5c0853b4b6891473014a5624f8e4965
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2020-11-21 09:48:58 +0000

    Corrected typo in exchanges/urls.py

--- exchanges/urls.py
+++ exchanges/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from .views import exchanges_page
 
 urlpatterns = [
-    url(r'^exchanges_page/$', exchanges_page, name='exchanges_page'),    
+    url(r'^exchanges_page/$', exchanges_page, name='exchanges_page'),  
 ]
