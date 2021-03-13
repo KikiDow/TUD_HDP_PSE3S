241270583f6a00b5c2c1cf9e72a866514530f715
EC2 Default User <ec2-user@ip-172-31-0-153.ec2.internal>
2021-03-13 08:26:39 +0000

    Implemented server-side validation and corrected error not re-calculating total claim amount on edit_allowance_request  view in overtime/views.py ensuring user selects at least one allowance before form will be submitted.

--- overtime/views.py
+++ overtime/views.py
@@ -168,6 +168,9 @@ def edit_allowance_request(request, pk):
     if request.method == "POST":
         edit_allow_req_form = AllowancesRequestForm(request.POST, request.FILES, instance=allow_req_for_editing)
         if edit_allow_req_form.is_valid():
+            if edit_allow_req_form.instance.claiming_breakfast_allowance == False and edit_allow_req_form.instance.claiming_dinner_allowance == False and edit_allow_req_form.instance.claiming_tea_allowance == False and edit_allow_req_form.instance.claiming_plain_clothes_allowance == False and edit_allow_req_form.instance.claiming_food_for_prisoner_expense == False:
+                messages.error(request, "You must select at least one allowance to make a valid allowance request.")
+                return render(request, "submit_allowance_request.html", {'edit_allow_req_form': edit_allow_req_form})
             if edit_allow_req_form.instance.claiming_food_for_prisoner_expense == True:
                 if edit_allow_req_form.instance.food_for_prisoner_amount is None:
                     messages.error(request, "You must include the cost of the prisoner's meal to claim this expense.")
@@ -191,6 +194,22 @@ def edit_allowance_request(request, pk):
                     allow_req_for_editing.claim_total = total_claim_amount
                     allow_req_for_editing.save()
                     return redirect(view_allowance_request, allow_req_for_editing.pk)
+            else:
+                allow_req_for_editing = edit_allow_req_form.save()
+                total_claim_amount = 0.0
+                if allow_req_for_editing.claiming_breakfast_allowance == True:
+                    total_claim_amount += settings.BREAKFAST_ALLOWANCE
+                if allow_req_for_editing.claiming_dinner_allowance == True:
+                    total_claim_amount += settings.DINNER_ALLOWANCE
+                if allow_req_for_editing.claiming_tea_allowance == True:
+                    total_claim_amount += settings.TEA_ALLOWANCE
+                if allow_req_for_editing.claiming_plain_clothes_allowance == True:
+                    total_claim_amount += settings.PLAIN_CLOTHES_ALLOWANCE
+                if allow_req_for_editing.claiming_food_for_prisoner_expense == True:
+                    total_claim_amount += allow_req_for_editing.food_for_prisoner_amount                    
+                allow_req_for_editing.claim_total = total_claim_amount
+                allow_req_for_editing.save()
+                return redirect(view_allowance_request, allow_req_for_editing.pk)
     else:
         edit_allow_req_form = AllowancesRequestForm(instance=allow_req_for_editing)
     return render(request, "edit_allowance_request.html", {'allow_req_for_editing': allow_req_for_editing, 'edit_allow_req_form': edit_allow_req_form})
